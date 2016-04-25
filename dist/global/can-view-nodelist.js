643e10996fd9dfb1e924adb5999a933867255cd6
David Luecke <daff@neyeon.com>
2016-04-25 14:58:28 -0700

    3.0.0-pre.1

---
+++ dist/global/can-view-nodelist.js
@@ -0,0 +1,310 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		modules[moduleName] = module && module.exports ? module.exports : result;
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-view-nodelist@3.0.0-pre.0#can-view-nodelist*/
+define('can-view-nodelist', function (require, exports, module) {
+    var CID = require('can-util/js/cid/cid');
+    var makeArray = require('can-util/js/make-array/make-array');
+    var each = require('can-util/js/each/each');
+    var domMutate = require('can-util/dom/mutate/mutate');
+    var canExpando = true;
+    try {
+        document.createTextNode('')._ = 0;
+    } catch (ex) {
+        canExpando = false;
+    }
+    var nodeMap = {}, textNodeMap = {}, expando = 'stache_' + Math.random(), _id = 0, id = function (node, localMap) {
+            var _textNodeMap = localMap || textNodeMap;
+            var id = readId(node, _textNodeMap);
+            if (id) {
+                return id;
+            } else {
+                if (canExpando || node.nodeType !== 3) {
+                    ++_id;
+                    return node[expando] = (node.nodeName ? 'element_' : 'obj_') + _id;
+                } else {
+                    ++_id;
+                    _textNodeMap['text_' + _id] = node;
+                    return 'text_' + _id;
+                }
+            }
+        }, readId = function (node, textNodeMap) {
+            if (canExpando || node.nodeType !== 3) {
+                return node[expando];
+            } else {
+                for (var textNodeID in textNodeMap) {
+                    if (textNodeMap[textNodeID] === node) {
+                        return textNodeID;
+                    }
+                }
+            }
+        }, splice = [].splice, push = [].push, itemsInChildListTree = function (list) {
+            var count = 0;
+            for (var i = 0, len = list.length; i < len; i++) {
+                var item = list[i];
+                if (item.nodeType) {
+                    count++;
+                } else {
+                    count += itemsInChildListTree(item);
+                }
+            }
+            return count;
+        }, replacementMap = function (replacements, idMap) {
+            var map = {};
+            for (var i = 0, len = replacements.length; i < len; i++) {
+                var node = nodeLists.first(replacements[i]);
+                map[id(node, idMap)] = replacements[i];
+            }
+            return map;
+        }, addUnfoundAsDeepChildren = function (list, rMap, foundIds) {
+            for (var repId in rMap) {
+                if (!foundIds[repId]) {
+                    list.newDeepChildren.push(rMap[repId]);
+                }
+            }
+        };
+    var nodeLists = {
+        id: id,
+        update: function (nodeList, newNodes) {
+            var oldNodes = nodeLists.unregisterChildren(nodeList);
+            newNodes = makeArray(newNodes);
+            var oldListLength = nodeList.length;
+            splice.apply(nodeList, [
+                0,
+                oldListLength
+            ].concat(newNodes));
+            if (nodeList.replacements) {
+                nodeLists.nestReplacements(nodeList);
+                nodeList.deepChildren = nodeList.newDeepChildren;
+                nodeList.newDeepChildren = [];
+            } else {
+                nodeLists.nestList(nodeList);
+            }
+            return oldNodes;
+        },
+        nestReplacements: function (list) {
+            var index = 0, idMap = {}, rMap = replacementMap(list.replacements, idMap), rCount = list.replacements.length, foundIds = {};
+            while (index < list.length && rCount) {
+                var node = list[index], nodeId = readId(node, idMap), replacement = rMap[nodeId];
+                if (replacement) {
+                    list.splice(index, itemsInChildListTree(replacement), replacement);
+                    foundIds[nodeId] = true;
+                    rCount--;
+                }
+                index++;
+            }
+            if (rCount) {
+                addUnfoundAsDeepChildren(list, rMap, foundIds);
+            }
+            list.replacements = [];
+        },
+        nestList: function (list) {
+            var index = 0;
+            while (index < list.length) {
+                var node = list[index], childNodeList = nodeMap[id(node)];
+                if (childNodeList) {
+                    if (childNodeList !== list) {
+                        list.splice(index, itemsInChildListTree(childNodeList), childNodeList);
+                    }
+                } else {
+                    nodeMap[id(node)] = list;
+                }
+                index++;
+            }
+        },
+        last: function (nodeList) {
+            var last = nodeList[nodeList.length - 1];
+            if (last.nodeType) {
+                return last;
+            } else {
+                return nodeLists.last(last);
+            }
+        },
+        first: function (nodeList) {
+            var first = nodeList[0];
+            if (first.nodeType) {
+                return first;
+            } else {
+                return nodeLists.first(first);
+            }
+        },
+        flatten: function (nodeList) {
+            var items = [];
+            for (var i = 0; i < nodeList.length; i++) {
+                var item = nodeList[i];
+                if (item.nodeType) {
+                    items.push(item);
+                } else {
+                    items.push.apply(items, nodeLists.flatten(item));
+                }
+            }
+            return items;
+        },
+        register: function (nodeList, unregistered, parent, directlyNested) {
+            CID(nodeList);
+            nodeList.unregistered = unregistered;
+            nodeList.parentList = parent;
+            nodeList.nesting = parent && typeof parent.nesting !== 'undefined' ? parent.nesting + 1 : 0;
+            if (parent) {
+                nodeList.deepChildren = [];
+                nodeList.newDeepChildren = [];
+                nodeList.replacements = [];
+                if (parent !== true) {
+                    if (directlyNested) {
+                        parent.replacements.push(nodeList);
+                    } else {
+                        parent.newDeepChildren.push(nodeList);
+                    }
+                }
+            } else {
+                nodeLists.nestList(nodeList);
+            }
+            return nodeList;
+        },
+        unregisterChildren: function (nodeList) {
+            var nodes = [];
+            each(nodeList, function (node) {
+                if (node.nodeType) {
+                    if (!nodeList.replacements) {
+                        delete nodeMap[id(node)];
+                    }
+                    nodes.push(node);
+                } else {
+                    push.apply(nodes, nodeLists.unregister(node, true));
+                }
+            });
+            each(nodeList.deepChildren, function (nodeList) {
+                nodeLists.unregister(nodeList, true);
+            });
+            return nodes;
+        },
+        unregister: function (nodeList, isChild) {
+            var nodes = nodeLists.unregisterChildren(nodeList, true);
+            if (nodeList.unregistered) {
+                var unregisteredCallback = nodeList.unregistered;
+                nodeList.replacements = nodeList.unregistered = null;
+                if (!isChild) {
+                    var deepChildren = nodeList.parentList && nodeList.parentList.deepChildren;
+                    if (deepChildren) {
+                        var index = deepChildren.indexOf(nodeList);
+                        if (index !== -1) {
+                            deepChildren.splice(index, 1);
+                        }
+                    }
+                }
+                unregisteredCallback();
+            }
+            return nodes;
+        },
+        after: function (oldElements, newFrag) {
+            var last = oldElements[oldElements.length - 1];
+            if (last.nextSibling) {
+                domMutate.insertBefore.call(last.parentNode, newFrag, last.nextSibling);
+            } else {
+                domMutate.appendChild.call(last.parentNode, newFrag);
+            }
+        },
+        replace: function (oldElements, newFrag) {
+            var selectedValue, parentNode = oldElements[0].parentNode;
+            if (parentNode.nodeName.toUpperCase() === 'SELECT' && parentNode.selectedIndex >= 0) {
+                selectedValue = parentNode.value;
+            }
+            if (oldElements.length === 1) {
+                domMutate.replaceChild.call(parentNode, newFrag, oldElements[0]);
+            } else {
+                nodeLists.after(oldElements, newFrag);
+                nodeLists.remove(oldElements);
+            }
+            if (selectedValue !== undefined) {
+                parentNode.value = selectedValue;
+            }
+        },
+        remove: function (elementsToBeRemoved) {
+            var parent = elementsToBeRemoved[0] && elementsToBeRemoved[0].parentNode;
+            each(elementsToBeRemoved, function (child) {
+                domMutate.removeChild.call(parent, child);
+            });
+        },
+        nodeMap: nodeMap
+    };
+    module.exports = nodeLists;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
